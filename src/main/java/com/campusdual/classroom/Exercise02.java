cf6d1a964d35333d329b4f6b82a831105165fb63
Juanfer <juanfer112@hotmail.com>
2024-11-24 12:24:46 +0100

    feature: solved math operation area and length

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -7,12 +7,24 @@ public class Exercise02 {
         double a, l;
 
         // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
+        a = calculateCircleArea(r);
 
         System.out.println("Area: " + a);
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
+        l = CalculateCircunference(r);
 
         System.out.println("Longitud:  " + l);
 
     }
+    // Método para calcular el área del círculo
+    private static double calculateCircleArea(double radius) {
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    // Método para calcular la circunferencia
+    private static double CalculateCircunference(double radius) {
+        return 2*Math.PI*radius;
+    }
+
 }
